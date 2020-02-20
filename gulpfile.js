c117a38d5e4e7f58e730695f14768636f1fdaa24
Yann VESTRIS <yann.vestris@polymontgroup.local>
2020-02-20 15:37:33 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const concat = require("gulp-concat");
+const prefix = require("gulp-autoprefixer");
+const notify = require("gulp-notify");
+const connect = require("gulp-connect-php");
+const browserSync = require("browser-sync").create();
+
+gulp.task("sass", done => {
+	gulp
+		.src("src/scss/**/*.scss")
+		.pipe(sass())
+		.pipe(prefix("last 2 versions"))
+		.pipe(concat("style.css"))
+		.pipe(gulp.dest("./"))
+		.pipe(notify("SCSS compiled !"));
+	done();
+});
+
+const reload = done => {
+	browserSync.reload();
+	done();
+};
+
+gulp.task("browser-sync", done => {
+	connect.server({}, () => {
+		browserSync.init({
+			proxy: "http://localhost/ofup2020",
+			open: false
+		});
+	});
+	gulp.watch("**/*.php", reload);
+	gulp.watch("inc/**/*.html", reload);
+	gulp.watch("src/scss/**/*.scss", reload);
+	done();
+});
+
+gulp.task(
+	"watch",
+	gulp.series("sass", "browser-sync", done => {
+		gulp.watch("src/scss/**/*.scss", gulp.series("sass"));
+		done();
+	})
+);
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>OFUP 2020</title>
+</head>
+
+<body>
+	<header>
+		<?= include("inc/header.inc.html") ?>
+	</header>
+	<main>
+		<?= include("inc/main.inc.html") ?>
+	</main>
+	<footer>
+		<?= include("inc/footer.inc.html") ?>
+	</footer>
+</body>
+
+</html>
